5584c2f19e0be980e148b8656b3e834ed7ee3b22
clemens33 <clemens.kr@gmail.com>
2024-09-19 12:28:19 +0200

    added upgrade workflow rework

--- .github/scripts/upgrade.sh
+++ .github/scripts/upgrade.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-# Upgrade MLflow to the latest version if it is not already up to date
+show_help() {
+  echo "Usage: ./upgrade.sh [OPTIONS]"
+  echo ""
+  echo "Options:"
+  echo "  -h, --help            Show this help message"
+  echo "  -s, --suffix          Specify the version suffix - e.g. 2.7.1-1, default set to 1"  
+}
 
-poetry export -f requirements.txt --output requirements.txt --without-hashes --with=genai
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -h|--help) show_help; exit 0;;    
+    -s|--suffix) VERSION_SUFFIX="$2"; shift; shift ;;
+    *) echo "Unknown parameter: $1"; show_help; exit 1 ;;
+  esac
+done
+
+# set defaults
+VERSION_SUFFIX=${VERSION_SUFFIX:-"1"}
+
+is_github_actions() {
+    [ -n "$GITHUB_ACTIONS" ]
+}
+
+if is_github_actions; then
+    echo "Running in GitHub Actions"
+    
+    cd "$GITHUB_WORKSPACE" || exit 1
+fi
+
+echo "Current directory: $(pwd)"
+
+poetry export -f requirements.txt --output requirements.txt --without-hashes --with=genai || exit 1
 REQUIREMENTS_FILE="requirements.txt"
 
 PYTHON_VERSION=$(grep -m1 'python_version >=' $REQUIREMENTS_FILE | awk -F'"' '{print $2}')
@@ -17,7 +46,7 @@ echo "Latest MLflow version: $LATEST_MLFLOW_VERSION"
 if [[ $MLFLOW_VERSION = $LATEST_MLFLOW_VERSION ]]; then
     echo "MLflow is already up to date"
     
-    exit 0
+    exit 1
 fi
 
 echo "Upgrading to latest versions $LATEST_MLFLOW_VERSION"
@@ -28,7 +57,7 @@ poetry add mlflow[genai]@latest --group genai
 
 # upgrade version in pyproject.toml
 PROJECT_VERSION=$(poetry version | awk '{print $2}')
-NEW_PROJECT_VERSION=$(echo $LATEST_MLFLOW_VERSION-1)
+NEW_PROJECT_VERSION=$(echo $LATEST_MLFLOW_VERSION-$VERSION_SUFFIX)
 poetry version $NEW_PROJECT_VERSION
 
 # upgrade mlflow tag version in docker/README.md from MLFLOW_VERSION to LATEST_MLFLOW_VERSION
@@ -38,6 +67,10 @@ sed -i "s/$MLFLOW_VERSION/$LATEST_MLFLOW_VERSION/g" docker/README.md
 sed -i "s/$MLFLOW_VERSION/$LATEST_MLFLOW_VERSION/g" charts/mlflow/Chart.yaml
 sed -i "s/$MLFLOW_VERSION/$LATEST_MLFLOW_VERSION/g" charts/mlflow-deployments-server/Chart.yaml
 
+if is_github_actions; then
+    echo "latest_mlflow_version=${LATEST_MLFLOW_VERSION}" >> $GITHUB_OUTPUT
+fi
 
+exit 0
 
 
